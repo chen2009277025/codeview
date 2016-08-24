c16b122fe79bdc3f46664a73d4ca55f6c5bb9746
chenjianhui <chenjianhui@meituan.com>
2016-08-24 17:54:14 +0800

    change

--- routes/index.js
+++ routes/index.js
@@ -2,8 +2,20 @@ var express = require('express');
 var router = express.Router();
 
 /* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+router.get(['/', "/index"], function (req, res, next) {
+    res.render('index', {title: 'Express'});
+});
+
+router.get('/about', function (req, res, next) {
+    res.render('about', {title: 'Express'});
+});
+
+router.get('/contact', function (req, res, next) {
+    res.render('contact', {title: 'Express'});
+});
+
+router.get('/pricing', function (req, res, next) {
+    res.render('pricing', {title: 'Express'});
 });
 
 module.exports = router;
